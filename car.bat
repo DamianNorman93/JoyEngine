e53390765c1572b5ee6b7e7c34fb752771dbed5e
DamianNorman93 <46695893+DamianNorman93@users.noreply.github.com>
2023-09-26 00:14:29 -0500

    Update car.bat

--- car.bat
+++ car.bat
@@ -1,5 +1,6 @@
 :: g++ -I src/include -L src/lib -o main main.cpp -lmingw32 -lSDL2main -lSDL2 
 :: g++ -I src/include -L src/lib -o main main.cpp -lmingw32 -lSDL2main -lSDL2 -lSDL2_ttf -mwindows
+Taskkill /im main.exe
 g++ -I src/include -L src/lib -o main main.cpp -lmingw32 -lSDL2main -lSDL2 -lSDL2_ttf -lSDL2_image 
 :: pause
 :: cls
